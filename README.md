f131a754e20caceee243a63ed8c8f27258ef3ddf
WulfMarius <WulfMarius@users.noreply.github.com>
2017-09-25 08:29:50 +0200

    initial version

---
+++ README.md
@@ -0,0 +1,11 @@
+Better-Placing
+
+
+This is a mod for **The Long Dark** by Hinterland Studio, Inc.
+
+
+It allows to rotate items with the mouse wheel while placing them.
+Hold down `Shift` fine-grained rotation control.
+
+
+Requires the [Mod Loader](https://github.com/zeobviouslyfakeacc/ModLoaderInstaller) to run.
---
+++ src/Patches.cs
@@ -0,0 +1,58 @@
+﻿using Harmony;
+using System.Reflection;
+using UnityEngine;
+
+namespace BetterPlacing
+{
+    internal class PlacingRotation
+    {
+        private static Quaternion rotation;
+
+        private static PlayerManager playerManager;
+        private static FieldInfo fieldInfo;
+
+        internal static void InitializeRotation(PlayerManager playerManager)
+        {
+            PlacingRotation.playerManager = playerManager;
+            fieldInfo = AccessTools.Field(playerManager.GetType(), "m_RotationInCameraSpace");
+
+            rotation = (Quaternion)fieldInfo.GetValue(playerManager);
+        }
+
+        internal static void Rotate(float xAngle, float yAngle, float zAngle)
+        {
+            rotation *= Quaternion.Euler(xAngle, yAngle, zAngle);
+
+            fieldInfo.SetValue(playerManager, rotation);
+        }
+    }
+
+    [HarmonyPatch(typeof(PlayerManager), "DoPositionCheck")]
+    class PlayerManager_DoPositionCheck
+    {
+        public static void Postfix(PlayerManager __instance)
+        {
+            if (Input.mouseScrollDelta.y == 0)
+            {
+                return;
+            }
+
+            float yAngle = Input.mouseScrollDelta.y;
+            if (!Input.GetKey(KeyCode.LeftShift) && !Input.GetKey(KeyCode.RightShift))
+            {
+                yAngle *= 5;
+            }
+
+            PlacingRotation.Rotate(0, yAngle, 0);
+        }
+    }
+
+    [HarmonyPatch(typeof(PlayerManager), "StartPlaceMesh")]
+    class PlayerManager_StartPlaceMesh
+    {
+        public static void Postfix(PlayerManager __instance)
+        {
+            PlacingRotation.InitializeRotation(__instance);
+        }
+    }
+}
